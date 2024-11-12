65dd2c1861c284f91d7097a89bb851028a080d1e
Yann Klein <yann.klein@me.com>
2024-11-12 18:48:38 +0900

    add map with clusters

---
+++ app/_components/MediaMap/MediaMap.tsx
@@ -0,0 +1,67 @@
+'use client';
+import {useRef} from 'react';
+import Map, { Layer, Source } from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { Media } from '@prisma/client';
+import type {GeoJSONSource, MapRef} from 'react-map-gl';
+import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
+
+
+export const MediaMap = ({ medias, accessToken }: { medias: Media[], accessToken: string }) => {
+  const mapStyle = 'mapbox://styles/mapbox/streets-v9';
+
+
+  const mapRef = useRef<MapRef>(null);
+
+  const onClick = (event: any) => {
+    const feature = event.features[0];
+    const clusterId = feature.properties.cluster_id;
+    if (mapRef.current === null) return;
+
+    const mapboxSource = mapRef.current.getSource('earthquakes') as GeoJSONSource;
+
+    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err || mapRef.current === null) {
+        return;
+      }
+
+      mapRef.current.easeTo({
+        center: feature.geometry.coordinates,
+        zoom: zoom as number,
+        duration: 500
+      });
+    });
+  };
+
+  return (
+    <>
+      <Map
+        initialViewState={{
+          latitude: 40.67,
+          longitude: -103.59,
+          zoom: 3
+        }}
+        mapStyle={mapStyle}
+        style={{width: "100vw", height: "600px"}}
+        mapboxAccessToken={accessToken}
+        interactiveLayerIds={[clusterLayer.id as string]}
+        onClick={onClick}
+        ref={mapRef}
+      >
+        <Source
+          id="earthquakes"
+          type="geojson"
+          data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={50}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+      </Map>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import prisma from '@/lib/prisma';
+import { MediaMap } from '@components/MediaMap';
+
+export default async function Home() {
+  const medias = await prisma.media.findMany();
+  const accessToken = process.env.MAPBOX_ACCESS_TOKEN as string; 
+
   return (
     <div>
-
+      <MediaMap medias={medias} accessToken={accessToken} />
     </div>
   );
 }
