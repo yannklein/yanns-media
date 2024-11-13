90cd98b4ad9a57f2ca58b6c56ba7089f8a96de64
Yann Klein <yann.klein@me.com>
2024-11-13 19:20:05 +0900

    create bounds center map and add padding

--- app/_components/MediaMap/MediaMap.tsx
+++ app/_components/MediaMap/MediaMap.tsx
@@ -1,25 +1,43 @@
 'use client';
-import {useRef} from 'react';
+import { useRef } from 'react';
 import Map, { Layer, Source } from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Media } from '@prisma/client';
-import type {GeoJSONSource, MapRef} from 'react-map-gl';
-import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
+import type { GeoJSONSource, MapMouseEvent, MapRef } from 'react-map-gl';
+import {
+  clusterLayer,
+  clusterCountLayer,
+  unclusteredPointLayer,
+} from './layers';
+import { GeoJSONFeature } from 'mapbox-gl';
+import bbox from '@turf/bbox';
 
+interface MapGLGeoJSONFeature extends GeoJSONFeature {
+  geometry: {
+    type: 'Point';
+    coordinates: [number, number];
+  };
+}
 
-export const MediaMap = ({ medias, accessToken }: { medias: Media[], accessToken: string }) => {
+export const MediaMap = ({
+  medias,
+  accessToken,
+}: {
+  medias: Media[];
+  accessToken: string;
+}) => {
   const mapStyle = 'mapbox://styles/mapbox/streets-v9';
-
-
   const mapRef = useRef<MapRef>(null);
 
-  const onClick = (event: any) => {
-    const feature = event.features[0];
-    const clusterId = feature.properties.cluster_id;
-    if (mapRef.current === null) return;
+  const onClick = (event: MapMouseEvent) => {
+    if (!event || !event.features) return;
+    const feature = event.features[0] as MapGLGeoJSONFeature;
 
-    const mapboxSource = mapRef.current.getSource('earthquakes') as GeoJSONSource;
+    const clusterId = feature?.properties?.cluster_id;
+    if (mapRef.current === null || clusterId === undefined) return;
+
+    const mapboxSource = mapRef.current.getSource('medias') as GeoJSONSource;
 
     mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
       if (err || mapRef.current === null) {
@@ -29,7 +47,7 @@ export const MediaMap = ({ medias, accessToken }: { medias: Media[], accessToken
       mapRef.current.easeTo({
         center: feature.geometry.coordinates,
         zoom: zoom as number,
-        duration: 500
+        duration: 500,
       });
     });
   };
@@ -40,21 +58,29 @@ export const MediaMap = ({ medias, accessToken }: { medias: Media[], accessToken
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [media.longitude, media.latitude]
-      }
-    }))
+        coordinates: [media.longitude, media.latitude],
+      },
+    })),
   };
+
+  const [minLng, minLat, maxLng, maxLat] = bbox(geojson as GeoJSON.GeoJSON);
 
   return (
     <>
       <Map
         initialViewState={{
-          latitude: 40.67,
-          longitude: -103.59,
-          zoom: 3
+          bounds: [minLng, minLat, maxLng, maxLat],
+          fitBoundsOptions: {
+            padding: {
+              top: 120,
+              bottom: 120,
+              left: 120,
+              right: 120,
+            },
+          },
         }}
         mapStyle={mapStyle}
-        style={{width: "100vw", height: "600px"}}
+        style={{ width: '100vw', height: '600px' }}
         mapboxAccessToken={accessToken}
         interactiveLayerIds={[clusterLayer.id as string]}
         onClick={onClick}
@@ -75,4 +101,4 @@ export const MediaMap = ({ medias, accessToken }: { medias: Media[], accessToken
       </Map>
     </>
   );
-}
+};
